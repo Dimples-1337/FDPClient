d47552e5009c7e445cb8a8702fecb0a86d90d613
Zywl <86253343+opZywl@users.noreply.github.com>
2021-08-16 14:02:13 -0300

    Create ServerSwitcher.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/ServerSwitcher.kt
@@ -0,0 +1,19 @@
+package net.ccbluex.liquidbounce.features.module.modules.client
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraft.client.gui.GuiMainMenu
+import net.minecraft.client.gui.GuiMultiplayer
+
+@ModuleInfo(
+        name ="ServerSwitcher",
+        description = "Switch the servers.",
+        category = ModuleCategory.CLIENT,
+        canEnable = false
+)
+class ServerSwitcher : Module() {
+    override fun onEnable() {
+        mc.displayGuiScreen(GuiMultiplayer(GuiMainMenu()))
+    }
+}
