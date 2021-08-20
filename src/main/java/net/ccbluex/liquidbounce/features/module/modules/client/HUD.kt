1e16f26fb3ac97babc6eebd442284b241ff35291
Zywl <86253343+opZywl@users.noreply.github.com>
2021-08-20 15:36:08 -0300

    Update HUD.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/HUD.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/HUD.kt
@@ -26,6 +26,8 @@ class HUD : Module() {
     val chatRectValue = BoolValue("ChatRect", true)
     val chatCombineValue = BoolValue("ChatCombine", true)
     val chatAnimValue = BoolValue("ChatAnimation", true)
+    val chatAnimationSpeedValue = FloatValue("Chat-AnimationSpeed", 0.1F, 0.01F, 0.1F)
+    val invEffectOffset = BoolValue("InvEffect-Offset", false)
 
     @EventTarget
     fun onRender2D(event: Render2DEvent) {
