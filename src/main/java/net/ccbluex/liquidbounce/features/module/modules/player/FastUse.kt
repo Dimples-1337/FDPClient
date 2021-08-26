44b9a3b1b7a1c5de2fe853c00d7096476c950485
ll11l1lIllIl1lll <88377095+ll11l1lIllIl1lll@users.noreply.github.com>
2021-08-26 21:53:56 +0800

    Minemora FastUse

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
@@ -21,7 +21,7 @@ import net.minecraft.network.play.client.C03PacketPlayer
 
 @ModuleInfo(name = "FastUse", category = ModuleCategory.PLAYER)
 class FastUse : Module() {
-    private val modeValue = ListValue("Mode", arrayOf("Instant", "Timer", "CustomDelay", "DelayedInstant"), "DelayedInstant")
+    private val modeValue = ListValue("Mode", arrayOf("Instant", "Timer", "CustomDelay", "DelayedInstant","MinemoraTest"), "DelayedInstant")
     private val timerValue = FloatValue("Timer", 1.22F, 0.1F, 2.0F).displayable { modeValue.get().equals("Timer",true) }
     private val durationValue = IntegerValue("InstantDelay", 14, 0, 35).displayable { modeValue.get().equals("DelayedInstant",true) }
     private val delayValue = IntegerValue("CustomDelay", 0, 0, 300).displayable { modeValue.get().equals("CustomDelay",true) }
@@ -64,6 +64,17 @@ class FastUse : Module() {
                     usedTimer = true
                 }
 
+                                "MinemoraTest" -> {
+                    mc.timer.timerSpeed = 0.5
+                                    usedTimer = true
+                                    if(mc.thePlayer.ticksExisted % 2 == 0){
+                                    repeat(2) {
+                    mc.netHandler.addToSendQueue(C03PacketPlayer(mc.thePlayer.onGround))
+                                    }
+                                    }
+                    
+                }
+                
                 "customdelay" -> {
                     if (!msTimer.hasTimePassed(delayValue.get().toLong()))
                         return
