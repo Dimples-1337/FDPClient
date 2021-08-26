58d38a0483c08764e6072cfdbb367801f383b614
ll11l1lIllIl1lll <88377095+ll11l1lIllIl1lll@users.noreply.github.com>
2021-08-26 22:57:43 +0800

    Update TPCommand.kt
    我是伞兵

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/TPCommand.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/TPCommand.kt
@@ -1,10 +1,8 @@
 package net.ccbluex.liquidbounce.features.command.commands
 
 import net.ccbluex.liquidbounce.features.command.Command
-import kotlin.math.cos
-import kotlin.math.sin
 
-class ClipCommand : Command("tp", emptyArray()) {
+class TPCommand : Command("tp", emptyArray()) {
     override fun execute(args: Array<String>) {
         if (args.size > 2) {
             val tpX: Double
@@ -19,7 +17,7 @@ class ClipCommand : Command("tp", emptyArray()) {
                 return
             }
 
-mc.thePlayer.setPosition(tpX, tpY, tpZ)
+            mc.thePlayer.setPosition(tpX, tpY, tpZ)
 
             return
         }
@@ -27,8 +25,5 @@ mc.thePlayer.setPosition(tpX, tpY, tpZ)
         chatSyntax("tp <x> <y> <z>")
     }
 
-    override fun tabComplete(args: Array<String>): List<String> {
-        return emptyList()
 
-    }
 }
