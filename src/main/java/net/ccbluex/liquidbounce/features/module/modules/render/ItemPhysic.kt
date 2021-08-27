8234c0af728aa851f9920e3b2e452cd460216772
Zywl <86253343+opZywl@users.noreply.github.com>
2021-08-27 19:07:28 -0300

    Create ItemPhysic.kt
    very easy just add PhysicUtils #181 and it will work

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ItemPhysic.kt
@@ -0,0 +1,6 @@
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+
+@ModuleInfo(name = "ItemPhysic", category = ModuleCategory.RENDER)
+class ItemPhysic : Module()
