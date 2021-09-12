beefef1674315539076cd5d9d6c85d80f1eed456
Zywl <86253343+opZywl@users.noreply.github.com>
2021-09-12 14:42:46 -0300

    Create ServerSwitch.kt
    ez change servers easy

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/ServerSwitch.kt
@@ -0,0 +1,19 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/UnlegitMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.client
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraft.client.gui.GuiMainMenu
+import net.minecraft.client.gui.GuiMultiplayer
+
+@ModuleInfo(name = "ServerSwitch", category = ModuleCategory.CLIENT)
+class ServerSwitcher : Module() {
+    override fun onEnable() {
+        mc.displayGuiScreen(GuiMultiplayer(GuiMainMenu())
+    }
+}
