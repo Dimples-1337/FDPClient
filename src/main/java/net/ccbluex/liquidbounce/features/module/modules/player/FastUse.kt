3eb67cfb056e1d89124c5410ce35d413351cf01f
ll11l1lIllIl1lll <88377095+ll11l1lIllIl1lll@users.noreply.github.com>
2021-09-27 12:49:51 +0800

    Update FastUse.kt
    NCP=delayedinstant

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/FastUse.kt
@@ -21,7 +21,7 @@ import net.minecraft.network.play.client.C03PacketPlayer
 
 @ModuleInfo(name = "FastUse", category = ModuleCategory.PLAYER)
 class FastUse : Module() {
-    private val modeValue = ListValue("Mode", arrayOf("Instant", "Timer", "CustomDelay", "DelayedInstant","MinemoraTest","NCP","AAC","NewAAC"), "DelayedInstant")
+    private val modeValue = ListValue("Mode", arrayOf("Instant", "Timer", "CustomDelay", "DelayedInstant","MinemoraTest","AAC","NewAAC"), "DelayedInstant")
     private val timerValue = FloatValue("Timer", 1.22F, 0.1F, 2.0F).displayable { modeValue.equals("Timer") }
     private val durationValue = IntegerValue("InstantDelay", 14, 0, 35).displayable { modeValue.equals("DelayedInstant") }
     private val delayValue = IntegerValue("CustomDelay", 0, 0, 300).displayable { modeValue.equals("CustomDelay") }
@@ -80,14 +80,6 @@ class FastUse : Module() {
                 "timer" -> {
                     mc.timer.timerSpeed = timerValue.get()
                     usedTimer = true
-                }
-                
-                "ncp" -> if (mc.thePlayer.itemInUseDuration > 14) {
-                     repeat(20) {
-                        mc.netHandler.addToSendQueue(C03PacketPlayer(mc.thePlayer.onGround))
-                    }
-
-                    mc.playerController.onStoppedUsingItem(mc.thePlayer)
                 }
 
                 "minemoratest" -> {
