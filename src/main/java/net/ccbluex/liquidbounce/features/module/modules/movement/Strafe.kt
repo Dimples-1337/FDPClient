179c95ac4b230acf1f9f7819eb29efabb4a40ff4
ll11l1lIllIl1lll <88377095+ll11l1lIllIl1lll@users.noreply.github.com>
2021-09-30 13:02:32 +0800

    Update Strafe.kt
    yorik100's StrafeRemade.js

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Strafe.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/Strafe.kt
@@ -16,9 +16,89 @@ import net.ccbluex.liquidbounce.utils.MovementUtils
 @ModuleInfo(name = "Strafe", category = ModuleCategory.MOVEMENT)
 class Strafe : Module() {
 
+    private var strengthValue= FloatValue("Strength", 0.5F, 0F, 1F)
+    private var noMoveStopValue = BoolValue("NoMoveStop", false)
+    private var onGroundStrafeValue = BoolValue("OnGroundStrafe", false)
+    private var allDirectionsJumpValue = BoolValue("AllDirectionsJump", false)
+    private var smoothValue = BoolValue("Smooth", false)
+    
+    private var wasDown: Boolean = false
+    private var jump: Boolean = false
+
     @EventTarget
-    fun onMotion(event: MotionEvent) {
-        if(event.eventState == EventState.POST)
-            MovementUtils.strafe()
+    fun onJump(event: JumpEvent) {
+        if (jump) {
+            event.cancelEvent()
+        }
+    }
+
+    override fun onEnable() {
+        wasDown = false
+    }
+
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        if (mc.thePlayer!!.onGround && mc.gameSettings.keyBindJump.isKeyDown && allDirectionsJumpValue.get() && (mc.thePlayer!!.movementInput.moveForward != 0F || mc.thePlayer!!.movementInput.moveStrafe != 0F) && !(mc.thePlayer!!.isInWater || mc.thePlayer!!.isInLava || mc.thePlayer!!.isOnLadder || mc.thePlayer!!.isInWeb)) {
+            if (mc.gameSettings.keyBindJump.isKeyDown) {
+                mc.gameSettings.keyBindJump.pressed = false
+                wasDown = true
+            }
+            val yaw = mc.thePlayer!!.rotationYaw
+            mc.thePlayer!!.rotationYaw = getMoveYaw()
+            mc.thePlayer!!.jump()
+            mc.thePlayer!!.rotationYaw = yaw
+            jump = true
+            if (wasDown) {
+                mc.gameSettings.keyBindJump.pressed = true
+                wasDown = false
+            }
+            if(smoothValue.get()){
+        MovementUtils.strafe();
+        mc.thePlayer.motionX = strengthValue.get() *mc.thePlayer.motionX + (1-strengthValue.get()) * mc.thePlayer.MotionX;
+        mc.thePlayer.motionY = strengthValue.get()*mc.thePlayer.motionY + (1-strengthValue.get()) * mc.thePlayer.motionY;
+        mc.thePlayer.motionZ = strengthValue.get()*mc.thePlayer.motionZ + (1-strengthValue.get()) * mc.thePlayer.MotionZ;
+            }
+        } else {
+            jump = false
+        }
+    }
+
+    @EventTarget
+    fun onStrafe(event: StrafeEvent) {
+        if(smoothValue.get()) return
+        val shotSpeed = sqrt((mc.thePlayer!!.motionX * mc.thePlayer!!.motionX) + (mc.thePlayer!!.motionZ * mc.thePlayer!!.motionZ))
+        val speed = (shotSpeed * strengthValue.get())
+        val motionX = (mc.thePlayer!!.motionX * (1 - strengthValue.get()))
+        val motionZ = (mc.thePlayer!!.motionZ * (1 - strengthValue.get()))
+        if (!(mc.thePlayer!!.movementInput.moveForward != 0F || mc.thePlayer!!.movementInput.moveStrafe != 0F)) {
+            if (noMoveStopValue.get()) {
+                mc.thePlayer!!.motionX = 0.0
+                mc.thePlayer!!.motionZ = 0.0
+            }
+            return
+        }
+        if (!mc.thePlayer!!.onGround || onGroundStrafeValue.get()) {
+            val yaw = getMoveYaw()
+            mc.thePlayer!!.motionX = (((-sin(Math.toRadians(yaw.toDouble())) * speed) + motionX))
+            mc.thePlayer!!.motionZ = (((cos(Math.toRadians(yaw.toDouble())) * speed) + motionZ))
+        }
+    }
+
+
+    private fun getMoveYaw(): Float {
+        var moveYaw = mc.thePlayer!!.rotationYaw
+        if (mc.thePlayer!!.moveForward != 0F && mc.thePlayer!!.moveStrafing == 0F) {
+            moveYaw += if(mc.thePlayer!!.moveForward > 0) 0 else 180
+        } else if (mc.thePlayer!!.moveForward != 0F && mc.thePlayer!!.moveStrafing != 0F) {
+            if (mc.thePlayer!!.moveForward > 0) {
+                moveYaw += if (mc.thePlayer!!.moveStrafing > 0) -45 else 45
+            } else {
+                moveYaw -= if (mc.thePlayer!!.moveStrafing > 0) -45 else 45
+            }
+            moveYaw += if(mc.thePlayer!!.moveForward > 0) 0 else 180
+        } else if (mc.thePlayer!!.moveStrafing != 0F && mc.thePlayer!!.moveForward == 0F) {
+            moveYaw += if(mc.thePlayer!!.moveStrafing > 0) -90 else 90
+        }
+        return moveYaw
     }
 }
