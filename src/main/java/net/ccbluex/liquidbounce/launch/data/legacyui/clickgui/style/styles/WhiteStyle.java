680c46fe59728689344b928b33124ef20e937e0a
Zywl <86253343+opZywl@users.noreply.github.com>
2022-01-16 11:40:44 -0300

    Update WhiteStyle.java

--- src/main/java/net/ccbluex/liquidbounce/launch/data/legacyui/clickgui/style/styles/WhiteStyle.java
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/legacyui/clickgui/style/styles/WhiteStyle.java
@@ -75,7 +75,7 @@ public class WhiteStyle extends Style {
 
     @Override
     public void drawButtonElement(int mouseX, int mouseY, ButtonElement buttonElement) {
-        Gui.drawRect(buttonElement.getX() - 1, buttonElement.getY() - 1, buttonElement.getX() + buttonElement.getWidth() + 1, buttonElement.getY() + buttonElement.getHeight() + 1, hoverColor(buttonElement.getColor() != Integer.MAX_VALUE ? new Color(7, 152, 252) : new Color(217,217,217), buttonElement.hoverTime).getRGB());
+        Gui.drawRect(buttonElement.getX() - 1, buttonElement.getY() - 1, buttonElement.getX() + buttonElement.getWidth() + 1, buttonElement.getY() + buttonElement.getHeight() + 1, hoverColor(buttonElement.getColor() != Integer.MAX_VALUE ? new Color(14, 159, 255) : new Color(217,217,217), buttonElement.hoverTime).getRGB());
 
         GlStateManager.resetColor();
 
@@ -128,7 +128,7 @@ public class WhiteStyle extends Style {
                             mc.getSoundHandler().playSound(PositionedSoundRecord.create(new ResourceLocation("gui.button.press"), 1.0F));
                         }
 
-                        Fonts.font35.drawString(text, moduleElement.getX() + moduleElement.getWidth() + 6, moduleElement.slowlySettingsYPos + 2, ((BoolValue) value).get() ? Color.BLACK.getRGB() : Integer.MAX_VALUE);
+                        Fonts.font35.drawString(text, moduleElement.getX() + moduleElement.getWidth() + 6, moduleElement.slowlySettingsYPos + 2, ((BoolValue) value).get() ? Color.BLACK.getRGB() : Color.DARK_GRAY.getRGB());
                         moduleElement.slowlySettingsYPos += 11;
                     } else if (value instanceof ListValue) {
                         final ListValue listValue = (ListValue) value;
@@ -162,7 +162,7 @@ public class WhiteStyle extends Style {
                                 }
 
                                 GlStateManager.resetColor();
-                                Fonts.font35.drawString("- " + valueOfList, moduleElement.getX() + moduleElement.getWidth() + 6, moduleElement.slowlySettingsYPos + 2, listValue.get() != null && listValue.get().equalsIgnoreCase(valueOfList) ? Color.BLACK.getRGB() : Integer.MAX_VALUE);
+                                Fonts.font35.drawString("- " + valueOfList, moduleElement.getX() + moduleElement.getWidth() + 6, moduleElement.slowlySettingsYPos + 2, listValue.get() != null && listValue.get().equalsIgnoreCase(valueOfList) ? Color.BLACK.getRGB() : Color.DARK_GRAY.getRGB());
                                 moduleElement.slowlySettingsYPos += Fonts.font35.FONT_HEIGHT + 1;
                             }
                         }
@@ -172,7 +172,7 @@ public class WhiteStyle extends Style {
                         }
                     } else if (value instanceof FloatValue) {
                         final FloatValue floatValue = (FloatValue) value;
-                        final String text = value.getName() + "§f: " + round(floatValue.get());
+                        final String text = value.getName() + ": " + round(floatValue.get());
                         final float textWidth = Fonts.font35.getStringWidth(text);
 
                         if (moduleElement.getSettingsWidth() < textWidth + 8)
@@ -187,7 +187,7 @@ public class WhiteStyle extends Style {
                         moduleElement.slowlySettingsYPos += 19;
                     } else if (value instanceof IntegerValue) {
                         final IntegerValue integerValue = (IntegerValue) value;
-                        final String text = value.getName() + "§f: " + (value instanceof BlockValue ? BlockUtils.getBlockName(integerValue.get()) + " (" + integerValue.get() + ")" : integerValue.get());
+                        final String text = value.getName() + ": " + (value instanceof BlockValue ? BlockUtils.getBlockName(integerValue.get()) + " (" + integerValue.get() + ")" : integerValue.get());
                         final float textWidth = Fonts.font35.getStringWidth(text);
 
                         if (moduleElement.getSettingsWidth() < textWidth + 8)
@@ -265,7 +265,7 @@ public class WhiteStyle extends Style {
 
                         moduleElement.slowlySettingsYPos += 11;
                     } else {
-                        final String text = value.getName() + "§f: " + value.get();
+                        final String text = value.getName() + ": " + value.get();
                         final float textWidth = Fonts.font35.getStringWidth(text);
 
                         if (moduleElement.getSettingsWidth() < textWidth + 8)
